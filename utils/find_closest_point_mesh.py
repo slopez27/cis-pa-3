2d3d366ec17d7c7b566d6e0941fb38f0d9404cc4
slopez27 <slopez27@jhu.edu>
2024-11-08 16:54:59 -0500

    idea for find closest point using barycentric approach does not do a lot of edge cases or advanced things

--- utils/find_closest_point_mesh.py
+++ utils/find_closest_point_mesh.py
@@ -1,15 +1,48 @@
 import numpy as np
 from point3d import Point3D
 
+# right now we are starting with the barycentric form discussed 
+# in Dr. Taylor's slides called Finding point-pairs (FindClosestPoint(a, [p, q, r]))
+
 class FindClosestPointMesh:
     def __init__(self, point: Point3D, triangle: list[int], vertices: list[Point3D]):
-        self.point = point.to_array()           # point
+        self.a = point.to_array()               # point
         self.triangle = triangle                # triangle is a list of 3 vertex indices
         self.vertices = vertices                # list of all vertices
 
-        v0, v1, v2 = [vertices[i] for i in triangle]
+        p, q, r = [vertices[i] for i in triangle]
 
-        self.v0 = v0.to_array()                 # first vertex point as an array
-        self.v1 = v1.to_array()                 # second vertex point as an array
-        self.v2 = v2.to_array()                 # third vertex point as an array
+        self.p = p.to_array()                 # first vertex point as an array
+        self.q = q.to_array()                 # second vertex point as an array
+        self.r = r.to_array()                 # third vertex point as an array
+
+    def compute_vectors(self):
+        ab = self.q - self.p
+        ac = self.r - self.p
+        ap = self.a - self.p
+
+        return ab, ac, ap
+    
+    def compute_dot_products(self):
+        ab, ac, ap = self.compute_vectors()
+
+        d1 = np.dot(ab, ap)
+        d2 = np.dot(ac, ap)
+        d3 = np.dot(ab, ab)
+        d4 = np.dot(ac, ab)
+        d5 = np.dot(ac, ac)
+
+        return d1, d2, d3, d4, d5
+    
+    def barycentric_coordinate_check(self):
+        d1, d2, d3, d4, d5 = self.compute_dot_products()
+        if d1 <= 0 and d2 <= 0:
+            return self.p
+        elif d3 <= d1 and d4 <= d2:
+            return self.q
+        elif d5 <= d2 and d4 <= d1:
+            return self.r
+        
+if __name__ == "__main__":
+    pass
 
