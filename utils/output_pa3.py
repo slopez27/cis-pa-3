ca9c5ed5bcc2591659d74869d9bc969bcb650669
slopez27 <slopez27@jhu.edu>
2024-11-08 16:24:51 -0500

    create files

---
+++ utils/output_pa3.py
@@ -0,0 +1,10 @@
+"""
+order in files for this assignment:
+    read input
+    find closest point on the mesh
+    point cloud registration
+    transformation estimation
+    closest point matching
+    output pa3 info
+
+"""
