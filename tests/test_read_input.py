98925fc540ae93119d3ed667e4ea26ce80e9f20e
Diana Leigh Neste <dlneste431@gmail.com>
2024-11-08 17:24:58 -0500

    read mesh

--- tests/test_read_input.py
+++ tests/test_read_input.py
@@ -4,7 +4,7 @@ import os
 import numpy as np
 import sys
 sys.path.append('../utils/')
-from read_input import ReadBody
+from read_input import ReadBody, ReadMesh
 
 class TestReadBody(unittest.TestCase):
 
@@ -17,6 +17,27 @@ class TestReadBody(unittest.TestCase):
         self.assertEqual(reader.tip,[0.0, 0.0, -100.0])
         print("Passed test_read_body!")
 
+class TestReadMesh(unittest.TestCase):
+    def setUp(self):
+        # Create a temporary file for testing
+        self.temp_file = tempfile.NamedTemporaryFile(delete=False, mode='w+')
+        self.temp_file.write("5\n")  # Sample data
+        self.temp_file.write("1.0 2.0 3.0\n" * 5)  # N_verticies = 5
+        self.temp_file.write("3\n")  # Sample data
+        self.temp_file.write("8 9 10 -1 4 5\n" * 3)  # N_triangles = 3
+        self.temp_file.seek(0)
+        self.temp_file.close()
+
+    def tearDown(self):
+        os.remove(self.temp_file.name)
+
+    def test_read_mesh(self):
+        reader = ReadMesh(self.temp_file.name)
+        self.assertEqual(reader.N_verticies, 5)
+        self.assertEqual(reader.N_triangles, 3)
+        self.assertEqual(reader.triangles_indicies, [[8, 9, 10], [8, 9, 10], [8, 9, 10]])
+        self.assertEqual(reader.neighbors_indicies, [[-1, 4, 5], [-1, 4, 5], [-1, 4, 5]])
+        print("Passed test_read_mesh!")
 
 if __name__ == "__main__":
     unittest.main()
