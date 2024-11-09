8aa4623685a443e7a80044f55a01bf47c1393e4a
slopez27 <slopez27@jhu.edu>
2024-11-09 11:03:40 -0500

    finished brute force closest point mesh

--- utils/find_closest_point_mesh.py
+++ utils/find_closest_point_mesh.py
@@ -9,16 +9,20 @@ class FindClosestPointMesh:
         read_mesh_file = ReadMesh(filename)
 
         N_vertices = read_mesh_file.N_vertices
-        vertices = read_mesh_file.vertices
+        self.vertices = read_mesh_file.vertices
         N_triangles = read_mesh_file.N_triangles
 
-        triangles_indices = read_mesh_file.triangles_indices
+        self.triangles_indices = read_mesh_file.triangles_indices
         neighbors_indices = read_mesh_file.neighbors_indices
 
         self.points = points
 
     def iterate(self):
+        closest_points = []
         for point in self.points:
+            find = FindClosestPointTriangle(point, self.triangles_indices, self.vertices)
+            closest_points.append(find.check_barycentric_coordinates())
+        return closest_points
             
 
 
