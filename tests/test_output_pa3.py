f921d770ce2555172c5af5b46d4309814a8c593f
Diana Leigh Neste <dlneste431@gmail.com>
2024-11-10 15:09:14 -0500

    output test

--- tests/test_output_pa3.py
+++ tests/test_output_pa3.py
@@ -1,6 +1,30 @@
+import unittest
+import tempfile
+import os
+import numpy as np
+import sys
+sys.path.append('../utils/')
+import filecmp
+from output_pa3 import output
+
 N_samps = 2
-letter = 'pa3-a'
+letter = 'pa3-test'
 d_k = [[1,2,3],[4,5,6]]
 s_k = [[0,0,0],[0,0,0]]
 test = output(N_samps, letter, d_k, s_k)
 test.write_to_file()
+
+class TestOutput(unittest.TestCase):
+    def test_output(self):
+        N_samps = 2
+        name = 'pa3-test'
+        path = "../outputs/pa3-test-Output.txt"
+        comp_path = "../outputs/pa3-test-comp-Output.txt"
+        d_k = [[1,2,3],[4,5,6]]
+        s_k = [[0,0,0],[0,0,0]]
+        test = output(N_samps, name, d_k, s_k)
+        test.write_to_file()
+        self.assertTrue(filecmp.cmp(path,comp_path,shallow=False))
+
+if __name__ == "__main__":
+    unittest.main()
